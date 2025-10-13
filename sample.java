b1d2b3070af52af3c4708a94dbe6faa5d7a44ec1
SHARAN SHETTY <143374479+Sharanshetty79@users.noreply.github.com>
2025-10-13 23:45:19 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,10 +2,10 @@ public class sample {
     public static void main (String[] args) {
         System.out.println("HELLO WORLD");
         System.out.println("devops");
-        System.out.println("Poll SCM");
         System.out.println("Done successfully");
     }
 }
+
 
 
 

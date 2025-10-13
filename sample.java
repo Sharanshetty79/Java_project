acefb9d07170df65b804ba6e9d28129c695b3a82
SHARAN SHETTY <143374479+Sharanshetty79@users.noreply.github.com>
2025-10-13 23:34:06 +0530

    poll scm

--- sample.java
+++ sample.java
@@ -2,7 +2,8 @@ public class sample {
     public static void main (String[] args) {
         System.out.println("HELLO WORLD");
         System.out.println("devops");
-        
+        System.out.println("Poll SCM");
     }
 }
+
 
